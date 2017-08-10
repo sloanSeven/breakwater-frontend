2de661bd5662fc2824a0fbe97989bd09414c3e76
stephenleonard <stephenleonard@prime.fios-router.home>
2017-08-10 16:30:28 -0400

    build dir

---
+++ bar/src2/Login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="Login">
+        <div className="App-header">
+          <h2>Welcome to BreakWater</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Login;
